d4a580a60b433fa12a867b0dc8a63d0665c791e3
vivek suryawanshi <viveklsuryawanshi@gmail.com>
2026-04-25 08:54:07 +0530

    daisyUI  , navbar addition

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    mainFields: ["module", "main", "browser"],
+  },
+  plugins: [react(), tailwindcss()],
+});
